b0174dc7644f7cd1e7e5681c861481cef30d5260
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-06-20 12:48:49 +0200

    Add Voiture class with attributes and methods

---
+++ classes.java
@@ -0,0 +1,38 @@
+public class Voiture {
+    // Attributes (Fields)
+    private String marque;
+    private String modele; // Assuming "voiture" meant the model of the car
+    private int annee;
+
+    // Constructor (Used to create a new Voiture object)
+    public Voiture(String marque, String modele, int annee) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+    }
+
+    // Getters and Setters (To access and modify the attributes)
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+}
