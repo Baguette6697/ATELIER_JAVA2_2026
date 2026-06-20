4d1bad4eea0e366cfc8fa1406b1c1247ccef3e77
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-06-20 13:08:47 +0200

    Add Voiture class with properties and constructor

---
+++ Voiture.java
@@ -0,0 +1,14 @@
+package exercice; // Important: This tells the project where the class lives
+
+public class Voiture {
+    public String marque;
+    public String modele;
+    public int annee;
+
+    // Constructor to make creation easy
+    public Voiture(String marque, String modele, int annee) {
+        this.marque = marque;
+        this.modele = modele;
+        this.annee = annee;
+    }
+}
